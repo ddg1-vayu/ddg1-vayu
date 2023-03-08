8b3a72a93bf9c8d4e80dd489a49b3cc2cfe28122
Vayu Dugar <vayu.dugar21@gmail.com>
2023-03-08 15:19:59 +0530

    Update snippets.php

--- snippets/snippets.php
+++ snippets/snippets.php
@@ -22,27 +22,31 @@ echo $output;
 
 
 // generate a string with the format “September 26th to October 4th 2022” from start and end dates in PHP with try & catch
-try {
-	// Create a DateTime object for the start date and another one for the end date
-	$start = new DateTime("2022-09-26");
-	$end = new DateTime("2022-10-04");
+function formatDateRange($start_date, $end_date) {
+  // Use a try-catch block to handle any exceptions
+  try {
+    // Create a DateTime object for the start date and another one for the end date
+    $start = new DateTime($start_date);
+    $end = new DateTime($end_date);
 
-	// Format the start date according to your desired output
-	$output = $start->format("F jS"); // September 26th
+    // Format the start date according to your desired output
+    $output = $start->format("F jS"); // September 26th
 
-	// Check if the start date and the end date have different months
-	if ($start->format("m") != $end->format("m")) {
-		// Different months
-		// Append "to" and the end date with full month name
-		$output .= " to " . $end->format("F jS Y"); // September 26th to October 4th 2022
-	} else {
-		// Same month
-		// Append "-" and the end date with only day name
-		$output .= " - " . $end->format("jS Y"); // September 26th - 30th 2022
-	}
+    // Check if the start date and the end date have different months
+    if ($start->format("m") != $end->format("m")) {
+      // Different months
+      // Append "to" and the end date with full month name
+      $output .= " to " . $end->format("F jS Y"); // September 26th to October 4th 2022
+    } else {
+      // Same month
+      // Append "-" and the end date with only day name
+      $output .= " - " . $end->format("jS Y"); // September 26th - 30th 2022
+    }
 
-	echo $output;
-} catch (Exception $e) {
-	// Handle any exceptions that may occur
-	echo "Error: " . $e->getMessage();
+    // Return the output
+    return $output;
+  } catch (Exception $e) {
+    // Handle any exceptions that may occur
+    return "Error: " . $e->getMessage();
+  }
 }
